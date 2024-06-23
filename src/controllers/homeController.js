4fc2e65a1258df326ca4ab564e85ccfd5762cbbc
RostislavIv <122882308+RostislavIv@users.noreply.github.com>
2024-06-23 15:20:27 +0300

    dynamic home page. Three latest recipes

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
+const recipeService = require("../services/recipeService");
+const { parseError } = require("../utils/errorUtils");
 
-router.get("/", (req, res) => {
-  res.render("home");
+router.get("/", async (req, res) => {
+  try {
+    const recipes = await recipeService.getThreeLatestRecipes();
+    res.render("home", { recipes });
+  } catch (err) {
+    const errors = parseError(err).errors;
+    res.render("home", { errors });
+  }
 });
 
 module.exports = router;
--- src/services/recipeService.js
+++ src/services/recipeService.js
@@ -4,3 +4,10 @@ exports.create = async (recipeData) => await Recipes.create(recipeData);
 
 exports.getAll = async () =>
   await Recipes.find().select("_id title image description").lean();
+
+exports.getThreeLatestRecipes = async () =>
+  await Recipes.find()
+    .select("_id title image description")
+    .sort({ createdAt: -1 })
+    .limit(3)
+    .lean();
