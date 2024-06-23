86e2c23c76587e4854da9ed4b7dddfda4e6717ed
RostislavIv <122882308+RostislavIv@users.noreply.github.com>
2024-06-23 15:42:08 +0300

    recommend pecipe

--- src/services/recipeService.js
+++ src/services/recipeService.js
@@ -13,3 +13,23 @@ exports.getThreeLatestRecipes = async () =>
     .lean();
 
 exports.getOne = async (recipeId) => await Recipes.findById(recipeId).lean();
+
+exports.recommend = async (recipeId, userId) => {
+  const recipe = await Recipes.findById(recipeId);
+
+  if (!recipe) {
+    throw new Error("Recipe not found");
+  }
+
+  if (recipe.owner.toString() === userId) {
+    throw new Error("You cannot recommend your own recipe");
+  }
+
+  if (recipe.recommendList.some((x) => x.toString() === userId)) {
+    throw new Error("You have already recommended this recipe");
+  }
+
+  recipe.recommendList.push(userId);
+
+  return await recipe.save();
+};
