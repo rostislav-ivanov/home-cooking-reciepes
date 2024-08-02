e4da2daaa46a7dd218e1fa4db1dd464cac17754d
RostislavIv <122882308+RostislavIv@users.noreply.github.com>
2024-08-02 11:33:20 +0300

    create docker-compose

--- src/config/configMongoose.js
+++ src/config/configMongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 // TODO: Add your connection string here
-const connectionString = "mongodb://localhost:27017/homeCookingRecipesDB";
+const connectionString =
+  process.env.MONGO_URI || "mongodb://localhost:27017/homeCookingRecipesDB";
 
 async function configMongoose() {
   try {
